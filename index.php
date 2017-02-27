97dbeaeb223551aaf9dfa7f1efa93f426131439c
vjfernandez <victor.fernandezgil@educa.madrid.org>
2017-02-27 15:28:54 +0100

    Añadido index.php
    Se ha añadido el fichero de index

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Despliegue en heroku </title>
+	<meta charset="UTF-8">
+</head>
+<body>
+<h1> Esto es un despliegue en heroku </h1>
+<p>
+	Añade un párrafo, y haz un pull request. <br>
+	Fdo: Víctor<br>
+	(Este repo está en <code>https://github.com/vjfernandez/despliegue-heroku</code>)
+</p>
+
+<!-- Añade otro párrafo aquí con un saludo y tu nombre sin apellidos 
+     y haz un pull request
+	 No añadas más ficheros al repo antes del pull request.
+	 
+	 Los ficheros que necesite heroku los creas en tu git, después del pull request.
+-->
+
+
+</body>
+</html>
